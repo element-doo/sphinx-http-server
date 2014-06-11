932848240ed2092fc2a8cc630df8c138dafdda76
Robert Petranovic <robi.petranovic@gmail.com>
2014-06-11 15:24:00 +0200

    WIP: just pushing everything

--- src/hr/element/sphinx/server/SphinxHttpServlet.java
+++ src/hr/element/sphinx/server/SphinxHttpServlet.java
@@ -4,7 +4,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +18,15 @@ import org.sphx.api.SphinxException;
 public class SphinxHttpServlet extends HttpServlet{
 
   private static final long serialVersionUID = 1L;
+  private SphinxSearch sphinx = null;
+
+  public void init(ServletConfig servletConfig) throws ServletException {
+	  final String hostname = servletConfig.getInitParameter("sphinx-hostname");
+	  final String _port = servletConfig.getInitParameter("sphinx-port");
+	  int port = Integer.parseInt(_port);
+
+      this.sphinx = new SphinxSearch(hostname, port);
+  }
 
 //  @Override
 //  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -46,6 +58,25 @@ public class SphinxHttpServlet extends HttpServlet{
 //    out.flush();
 //  }
 
+  
+  protected Map<String, Object> parseParams(HttpServletRequest req)
+  {
+    Map<String, Object> queryParams = new HashMap<String, Object>();
+    queryParams.put("limit",  null);
+    queryParams.put("offset", null);
+
+    for (Map.Entry<String, Object> i : queryParams.entrySet()) {
+      try {
+        i.setValue(Integer.parseInt(req.getParameter(i.getKey())));
+      } catch (Exception e) { }
+    }
+
+    final String order = req.getParameter("order");
+    queryParams.put("order", order == null ? null : order.split(","));
+    
+    return queryParams;
+  }
+
   @Override
   protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     InputStream inputStream = req.getInputStream();
@@ -54,7 +85,7 @@ public class SphinxHttpServlet extends HttpServlet{
     int bytesRead;
     ByteArrayOutputStream output = new ByteArrayOutputStream();
     while ((bytesRead = inputStream.read(buffer)) != -1){
-        output.write(buffer, 0, bytesRead);
+      output.write(buffer, 0, bytesRead);
     }
 
     String data = output.toString();
@@ -62,18 +93,24 @@ public class SphinxHttpServlet extends HttpServlet{
 
     System.out.println("\nPUT");
     System.out.println("data: " + data + "; index: " + index + "; content-type: " +req.getContentType() + "\n");
-
+    
     String json = new String();
 
     if (data != null && !data.equals("")) {
-      SphinxSearch sphinx = new SphinxSearch(index, data);
       try {
-        json = sphinx.search();
+    	Map<String, Object> queryParams = parseParams(req);
+        json = this.sphinx.search(
+        		index,
+        		data,
+        		(Integer) queryParams.get("limit"),
+        		(Integer) queryParams.get("offset"),
+        		(String[]) queryParams.get("order")
+        		);
       } catch (SphinxException e) {
         e.printStackTrace();
       }
     } else {
-      json = "No text in request body found";
+      json = "\"No text in request body found\"";
     }
 
     resp.setContentType("application/json");
--- src/hr/element/sphinx/server/SphinxSearch.java
+++ src/hr/element/sphinx/server/SphinxSearch.java
@@ -1,7 +1,13 @@
 package hr.element.sphinx.server;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.sphx.api.SphinxClient;
@@ -11,34 +17,95 @@ import org.sphx.api.SphinxResult;
 import org.sphx.api.SphinxWordInfo;
 
 public class SphinxSearch {
-
-  private final String index;
-  private final String text;
-
-  public SphinxSearch(final String index, final String text) {
-    this.index = index;
-    this.text = text;
+  protected final String hostname;
+  protected int port;
+	
+  public SphinxSearch(final String hostname, int port) {
+	this.hostname = hostname;
+	this.port = port;
   }
 
-  @SuppressWarnings("unchecked")
-  public String search () throws SphinxException{
+  public String search(final String index, final String text) throws SphinxException {
+	return this.search(index, text, null, null, null);
+  }
+  
+  public String search(final String index, final String text, Integer limit) throws SphinxException {
+	return this.search(index, text, limit, null, null);
+  }
+  
+  protected String parseOrder(String[] order)
+  {
+	List<String> result = new LinkedList<String>();
 
-    String host = "localhost";
-    int port = 9312;
+	if (order == null || order.length == 0)
+		order = ArrayUtils.add(order, "!@weight");
+
+	for (String ord : order) {
+	  boolean reversed = ord.length() > 0 && ord.charAt(0) == '!';
+	  final String key = reversed ? ord.substring(1) : ord;
+	  result.add(key + (reversed ? " DESC" : " ASC"));
+	}
+
+	return StringUtils.join(result, ", ");
+  }
+  
+  protected Object getAttribute(int type, Object value)
+  {
+      switch (type){
+        case SphinxClient.SPH_ATTR_MULTI:
+        case SphinxClient.SPH_ATTR_MULTI64:
+          long[] attrM = (long[]) value;
+          JSONArray attrMjs = new JSONArray();
+          for (int j = 0; j < attrM.length; j++)
+            attrMjs.add(attrM[j]);
+          return attrMjs;
+
+        case SphinxClient.SPH_ATTR_INTEGER:
+        case SphinxClient.SPH_ATTR_ORDINAL:
+        case SphinxClient.SPH_ATTR_FLOAT:
+        case SphinxClient.SPH_ATTR_BIGINT:
+        case SphinxClient.SPH_ATTR_STRING:
+          return value;
+
+        case SphinxClient.SPH_ATTR_TIMESTAMP:
+          Long iStamp = (Long) value;
+          Date date = new Date(iStamp.longValue()*1000);
+          return date.toString();
+
+        default:
+          return null;
+      }
+  }
+  
+  protected Map<String, Object> getAttributes(SphinxResult res, SphinxMatch match)
+  {
+	HashMap<String, Object> map = new HashMap<String, Object>();
+    for (int i = 0; i < res.attrNames.length; i++) {
+      map.put(res.attrNames[i], getAttribute(res.attrTypes[i], match.attrValues.get(i)));
+    }
+    return map;
+  }
+  
+  @SuppressWarnings("unchecked")
+  public String search (final String index, final String text, Integer limit, Integer offset, String[] order) throws SphinxException {
     int mode = SphinxClient.SPH_MATCH_ALL;
-    int offset = 0;
-    int limit = 20;
-    int sortMode = SphinxClient.SPH_SORT_RELEVANCE;
-    String sortClause = "";
+    int sortMode = SphinxClient.SPH_SORT_EXTENDED;
+    
+    int _limit = limit == null ? 20 : limit;
+    int _offset = offset == null ? 0 : offset;
+
+    String sortClause = parseOrder(order);
     String groupBy = "";
     String groupSort = "";
+    
+    System.out.println("sortClause: [" + sortClause + "]");
 
     SphinxClient cl = new SphinxClient();
 
-    cl.SetServer ( host, port );
+    cl.SetServer ( this.hostname, this.port );
     cl.SetWeights ( new int[] { 100, 1 } );
     cl.SetMatchMode ( mode );
-    cl.SetLimits ( offset, limit );
+    cl.SetLimits ( _offset, _limit );
     cl.SetSortMode ( sortMode, sortClause );
 
     if ( groupBy.length() > 0 )
@@ -61,68 +128,30 @@ public class SphinxSearch {
       SphinxWordInfo wordInfo = res.words[i];
       System.out.println ( "\t'" + wordInfo.word + "' found " + wordInfo.hits + " times in " + wordInfo.docs + " documents" );
     }
-
-    final JSONArray jsonArray = new JSONArray();
-    JSONObject match;
+    
+    final JSONObject result = new JSONObject();
+    final JSONArray matches = new JSONArray();
+    result.put("matches", matches);
+    result.put("count", res.totalFound);
 
     System.out.println ( "\nMatches:" );
-    for ( int i = 0; i < res.matches.length; i++ ){
-      SphinxMatch info = res.matches[i];
-      System.out.print ( (i+1) + ". id=" + info.docId + ", weight=" + info.weight );
+    for (int i = 0; i < res.matches.length; i++) {
+      SphinxMatch match = res.matches[i];
+      System.out.print ( (i+1) + ". id=" + match.docId + ", weight=" + match.weight );
 
       if ( res.attrNames == null || res.attrTypes == null )
         continue;
+      
+      Map<String, Object> attrs = getAttributes(res, match);
 
-      match = new JSONObject();
+      JSONObject jsobj = new JSONObject();
+      jsobj.put("weight", match.weight);
+      jsobj.put("pk", attrs.get("pk"));
 
-      match.put("weight", info.weight);
-
-      for ( int a = 0; a < res.attrNames.length; a++ ){
-
-        StringBuilder builder = new StringBuilder();
-        System.out.print ( ", " + res.attrNames[a] + "=" );
-
-        if ( res.attrTypes[a] == SphinxClient.SPH_ATTR_MULTI || res.attrTypes[a] == SphinxClient.SPH_ATTR_MULTI64 ){
-          System.out.print ( "(" );
-          long[] attrM = (long[]) info.attrValues.get(a);
-          if ( attrM!=null )
-            for ( int j = 0; j < attrM.length; j++ ){
-            if ( j != 0 ) { System.out.print ( "," ); builder.append(","); }
-            System.out.print ( attrM[j] );
-            builder.append(attrM[j]);
-          }
-          System.out.print ( ")" );
-
-        } else{
-          switch ( res.attrTypes[a] ){
-            case SphinxClient.SPH_ATTR_INTEGER:
-            case SphinxClient.SPH_ATTR_ORDINAL:
-            case SphinxClient.SPH_ATTR_FLOAT:
-            case SphinxClient.SPH_ATTR_BIGINT:
-            case SphinxClient.SPH_ATTR_STRING:
-              /* ints, longs, floats, strings.. print as is */
-              System.out.print ( info.attrValues.get(a) );
-              builder.append(info.attrValues.get(a));
-              break;
-
-            case SphinxClient.SPH_ATTR_TIMESTAMP:
-              Long iStamp = (Long) info.attrValues.get(a);
-              Date date = new Date ( iStamp.longValue()*1000 );
-              System.out.print ( date.toString() );
-              builder.append(date.toString());
-              break;
-
-            default:
-              System.out.print ( "(unknown-attr-type=" + res.attrTypes[a] + ")" );
-          }
-        }
-        System.out.print("");
-        match.put(res.attrNames[a], builder.toString());
-      }
-      jsonArray.add(match);
+      matches.add(jsobj);
       System.out.println();
     }
-    return jsonArray.toString();
-  }
 
+    return result.toString();
+  }
 }
